30dc121cab7bac036f222074dc81e88c9e2755f3
amesemyta1 <amesemyta@gmail.com>
2022-07-04 12:07:19 +0300

    livecode

---
+++ array/index.js
@@ -0,0 +1,97 @@
+/* ==================================== */
+/* ===> мутирующие методы массивов <=== */
+/* ===> изменяют исходный массив <===== */
+/* ==================================== */
+
+/* pop - удалит из массива последний элемент и вернет его */
+
+// c помощью метода pop достань последний элемент массива numbersList1 и помести его в переменную lastNumber
+// выведи в консоль эту переменную, а так же массив numbersList1 после выполнения pop
+
+const numbersList1 = [1, 2, 3, 4, 5];
+
+console.log('Before pop' + numbersList1);
+const lastNumber = numbersList1.pop();
+console.log('After pop' + numbersList1);
+
+/* push - добавит элемент в конец массива */
+
+// c помощью метода push добавь число 6 в конец массива numbersList2
+// выведи в консоль результат работы метода push, а так же массив numbersList2 после выполнения push
+
+const numbersList2 = [1, 2, 3, 4, 5];
+
+console.log('Before push' + numbersList2);
+const pushRes = numbersList2.push(666);
+console.log('After push' + numbersList2);
+
+/* shift - удалит из массива первый элемент и вернет его */
+
+// c помощью метода shift достань первый элемент массива numbersList3 и помести его в переменную firstNumber
+// выведи в консоль эту переменную, а так же массив numbersList2 после выполнения shift
+
+const numbersList3 = [1, 2, 3, 4, 5];
+
+console.log('Before shift' + numbersList3);
+const shiftRes = numbersList3.shift();
+console.log('After shift' + numbersList3);
+
+/* unshift - добавит элемент в начало массива */
+
+// c помощью метода unshift добавь число 0 в начало массива
+// выведи в консоль результат работы метода unshift, а так же массив numbersList4 после выполнения unshift
+
+const numbersList4 = [1, 2, 3, 4, 5];
+
+// put your code here
+
+/* ======================================= */
+/* ===> НЕ мутирующие методы массивов <=== */
+/* ===> НЕ изменяют исходный массив <===== */
+/* ======================================= */
+
+// начальный массив
+const anotherNumbersList = [5, 0, 8, 10, -4, 50, 220];
+
+/* метод filter */
+/* const newArr = arr.filter(callback) - создает и возвращает новый отфильтрованный массив.
+   ф-ция callback запустится по очереди для каждого элемента начального массива arr
+   в newArr попадут только те элементы arr, для которых callback(arr[i]) вернет true
+   callback(arr[i]) === true -> элемент будет добавлен в финальный массив
+   callback(arr[i]) === false -> элемент НЕ будет добавлен в финальный массив */
+
+// c помощью метода filter создай новый массив bigNumbers, в котором будут числа > 5 из anotherNumbersList
+// выведи bigNumbers в консоль
+
+// put your code here
+
+// c помощью метода filter создай новый массив evenPositions, в котором будут числа из четных позиций которые > 5
+// массива anotherNumbersList
+// выведи evenPositions в консоль
+
+// put your code here
+
+function callback(elem) {
+  return elem > 5;
+}
+
+const filterRes = anotherNumbersList.filter(function callback(elem) {
+  return elem > 5;
+});
+
+function resolver(elem) {
+  console.log(elem);
+}
+
+function sum(from, to, resolver) {
+  let sumRes = 0;
+
+  for (let i = from; i <= to; i += 1) {
+    sumRes += i;
+  }
+
+  resolver(sumRes);
+}
+
+// -- test data
+console.log(sum(1, 10, resolver));
