88d73b695b41da786544cb66250ccf53d73dc75b
amesemyta1 <amesemyta@gmail.com>
2022-07-18 15:36:57 +0300

    lvcod

---
+++ object3/index.js
@@ -0,0 +1,36 @@
+function compareObjects(obj1, obj2) {
+  const firstKey = Object.keys(obj1);
+  const secondKey = Object.keys(obj2);
+
+  if (firstKey.length !== secondKey.length) {
+    return false;
+  }
+
+  return secondKey.every(key => obj1(key) === obj2(key));
+}
+
+// examples
+const obj1 = {
+  name: 'Tom',
+  age: 17,
+};
+
+const obj2 = {
+  name: 'Bob',
+  age: 17,
+};
+
+const obj3 = {
+  name: 'Bob',
+  age: 17,
+  student: false,
+};
+
+const obj4 = {
+  name: 'Tom',
+  age: 17,
+};
+
+compareObjects(obj1, obj2); // ==> false
+compareObjects(obj2, obj3); // ==> false
+compareObjects(obj1, obj4); // ==> true
